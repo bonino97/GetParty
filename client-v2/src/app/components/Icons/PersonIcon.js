a370731d9c5c366edf777659e5c67d1f9dcca3e0
bonino97 <juanbonino97@gmail.com>
2021-08-21 21:15:56 -0300

    Working first version

--- client-v2/src/app/components/Icons/PersonIcon.js
+++ client-v2/src/app/components/Icons/PersonIcon.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PersonPinCircleSharpIcon from '@material-ui/icons/PersonPinCircleSharp';
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default ({ size, color, onClick }) => (
-  <PersonPinCircleSharpIcon onClick={onClick} style={{ fontSize: size, color }} />
+  <PersonPinCircleSharpIcon
+    onClick={onClick}
+    style={{ fontSize: size, color }}
+  />
 );
--- client-v2/src/app/services/firebaseService/firebaseService.js
+++ client-v2/src/app/services/firebaseService/firebaseService.js
@@ -1,4 +1,3 @@
-/* eslint import/no-extraneous-dependencies: off */
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
