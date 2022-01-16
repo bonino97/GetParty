9ecefd455953a7a4891e8e4a656bd5ed78fb9360
bonino97 <juanbonino97@gmail.com>
2022-01-16 14:01:10 -0300

    Move Client.

--- client/src/app/components/Icons/PersonIcon.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import PersonPinCircleSharpIcon from '@material-ui/icons/PersonPinCircleSharp';
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default ({ size, color, onClick }) => (
-  <PersonPinCircleSharpIcon onClick={onClick} style={{ fontSize: size, color }} />
-);
--- client/src/app/components/Icons/PinIcon.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import PlaceTwoTone from '@material-ui/icons/PlaceTwoTone';
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default ({ size, color, onClick }) => (
-  <PlaceTwoTone onClick={onClick} style={{ fontSize: size, color }} />
-);
