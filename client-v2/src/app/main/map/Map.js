2ddd4ffb1010af1bcb8d08a20fbf078b781bd0e9
bonino97 <juanbonino97@gmail.com>
2022-01-17 16:22:48 -0300

    Remove consolelogs

--- client-v2/src/app/main/map/Map.js
+++ client-v2/src/app/main/map/Map.js
@@ -171,7 +171,6 @@ const Map = ({ classes }) => {
             subscription={PIN_ADDED_SUBSCRIPTION}
             onSubscriptionData={({ subscriptionData }) => {
               const { pinAdded } = subscriptionData?.data;
-              console.log(pinAdded);
               dispatch({ type: 'CREATE_PIN', payload: pinAdded });
             }}
           />
@@ -179,7 +178,6 @@ const Map = ({ classes }) => {
             subscription={PIN_UPDATED_SUBSCRIPTION}
             onSubscriptionData={({ subscriptionData }) => {
               const { pinUpdated } = subscriptionData?.data;
-              console.log(pinUpdated);
               dispatch({ type: 'CREATE_COMMENT', payload: pinUpdated });
             }}
           />
@@ -187,7 +185,6 @@ const Map = ({ classes }) => {
             subscription={PIN_DELETED_SUBSCRIPTION}
             onSubscriptionData={({ subscriptionData }) => {
               const { pinDeleted } = subscriptionData?.data;
-              console.log(pinDeleted);
               dispatch({ type: 'DELETE_PIN', payload: pinDeleted });
             }}
           />
