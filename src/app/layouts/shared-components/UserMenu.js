4d9219e0fa505e253d7ea2202e981c73c7fcead3
bonino97 <juanbonino97@gmail.com>
2022-03-06 21:07:34 -0500

    Fix popover

--- src/app/layouts/shared-components/UserMenu.js
+++ src/app/layouts/shared-components/UserMenu.js
@@ -33,25 +33,14 @@ function UserMenu(props) {
 
   return (
     <>
-      <Button
-        className='min-h-40 min-w-40 px-0 md:px-16 py-0 md:py-6'
-        onClick={userMenuClick}
-      >
+      <Button className='min-h-40 min-w-40 px-0 md:px-16 py-0 md:py-6' onClick={userMenuClick}>
         {currentUser && (
           <>
-            <Avatar
-              className=''
-              alt='user photo'
-              src={
-                currentUser?.picture
-                  ? currentUser?.picture
-                  : 'assets/images/avatars/profile.jpg'
-              }
-            />
+            <Avatar className='' alt='user photo' src={currentUser?.picture} />
           </>
         )}
 
-        <Icon className='text-16 ml-12 hidden sm:flex' variant='action'>
+        <Icon className='text-16 ml-12 sm:flex' variant='action'>
           keyboard_arrow_down
         </Icon>
       </Button>
@@ -74,40 +63,28 @@ function UserMenu(props) {
       >
         {!currentUser ? (
           <>
-            <>
-              <MenuItem component={Link} to='/login' role='button'>
-                <ListItemIcon className='min-w-40'>
-                  <Icon>lock</Icon>
-                </ListItemIcon>
-                <ListItemText primary='Login' />
-              </MenuItem>
-              <MenuItem component={Link} to='/register' role='button'>
-                <ListItemIcon className='min-w-40'>
-                  <Icon>person_add</Icon>
-                </ListItemIcon>
-                <ListItemText primary='Register' />
-              </MenuItem>
-            </>
+            <MenuItem component={Link} to='/login' role='button'>
+              <ListItemIcon className='min-w-40'>
+                <Icon>lock</Icon>
+              </ListItemIcon>
+              <ListItemText primary='Login' />
+            </MenuItem>
+            <MenuItem component={Link} to='/register' role='button'>
+              <ListItemIcon className='min-w-40'>
+                <Icon>person_add</Icon>
+              </ListItemIcon>
+              <ListItemText primary='Register' />
+            </MenuItem>
           </>
         ) : (
           <>
-            <MenuItem
-              component={Link}
-              to='/pages/profile'
-              onClick={userMenuClose}
-              role='button'
-            >
+            <MenuItem component={Link} to='/profile' onClick={userMenuClose} role='button'>
               <ListItemIcon className='min-w-40'>
                 <Icon>account_circle</Icon>
               </ListItemIcon>
               <ListItemText primary='My Profile' />
             </MenuItem>
-            <MenuItem
-              component={Link}
-              to='/apps/mail'
-              onClick={userMenuClose}
-              role='button'
-            >
+            <MenuItem component={Link} to='/apps/mail' onClick={userMenuClose} role='button'>
               <ListItemIcon className='min-w-40'>
                 <Icon>mail</Icon>
               </ListItemIcon>
